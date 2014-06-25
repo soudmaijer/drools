d6bbdbd9ff1d3f476a338004f676ab9e3adadf93
Stephan Oudmaijer <soudmaijer@gmail.com>
2014-06-25 15:04:36 +0200

    Cleanup

--- src/main/java/com/oudmaijer/drools/FraudApplication.java
+++ src/main/java/com/oudmaijer/drools/FraudApplication.java
@@ -1,35 +1,23 @@
 package com.oudmaijer.drools;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.oudmaijer.drools.config.DroolsConfig;
-import com.oudmaijer.drools.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import static org.ajar.swaggermvcui.SwaggerSpringMvcUi.WEB_JAR_VIEW_RESOLVER_SUFFIX;
 
 @Configuration
 @EnableAutoConfiguration
-@Import({DroolsConfig.class, SwaggerConfig.class})
+@EnableSwagger
+@Import({DroolsConfig.class})
 public class FraudApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(FraudApplication.class);
-    }
-
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolverJsp() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(WEB_JAR_VIEW_RESOLVER_SUFFIX);
-        resolver.setOrder(0);
-        return resolver;
     }
 
     public static void main(String[] args) {
