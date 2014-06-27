7c44235b8041d36ad040659f38f0891b4482d357
Stephan Oudmaijer <soudmaijer@gmail.com>
2014-06-27 08:29:01 +0200

    Add histogram metric

--- src/main/java/com/oudmaijer/drools/fraud/FraudService.java
+++ src/main/java/com/oudmaijer/drools/fraud/FraudService.java
@@ -1,10 +1,13 @@
 package com.oudmaijer.drools.fraud;
 
+import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Metered;
 import com.oudmaijer.drools.fraud.model.Errors;
 import com.oudmaijer.drools.fraud.model.Order;
 import org.kie.api.KieBase;
 import org.kie.api.runtime.KieSession;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.boot.actuate.metrics.repository.MetricRepository;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -14,14 +17,19 @@ public class FraudService {
 
     private final KieBase kbase;
     private final FraudRepository fraudRepository;
+    private final MetricRepository metricRepository;
+    private final GaugeService gaugeService;
 
     @Inject
-    public FraudService(KieBase kbase, FraudRepository fraudRepository) {
+    public FraudService(KieBase kbase, FraudRepository fraudRepository, MetricRepository metricRepository, GaugeService gaugeService) {
         this.kbase = kbase;
         this.fraudRepository = fraudRepository;
+        this.metricRepository = metricRepository;
+        this.gaugeService = gaugeService;
     }
 
     @Metered
+    @ExceptionMetered
     public Errors check(Order order) {
         KieSession ksession = kbase.newKieSession();
 
@@ -35,6 +43,9 @@ public class FraudService {
         // Fire all rules and destroy session.
         ksession.fireAllRules();
         ksession.destroy();
+
+        // Update metrics
+        gaugeService.submit("histogram.fraud.check.order", errors.getErrors().size());
 
         // Return result
         return errors;
